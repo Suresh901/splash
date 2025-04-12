3a84dd88aabc6d63b32fa303797f3f9cf3931fe4
Suresh901 <sureshshrestha915@gmail.com>
2025-04-12 14:34:39 +0545

    home components added

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
+import SwimClassCards from "./components/SwimClassCards";
+import AboutSection from "./components/AboutSection";
+import OurClasses from "./components/OurClasses";
+import InstructorSlider from "./components/InstructorSlider";
+import Testimonial from "./components/Testimonial";
+import GallerySection from "./components/Masonry";
+import PricingAndStats from "./components/Pricing";
+import SwimmingSchool from "./components/SwimmingSchool";
+import Footer from "./components/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSection />
+      <SwimClassCards />
+      <AboutSection />
+      <OurClasses />
+      <InstructorSlider />
+      <Testimonial />
+      <GallerySection />
+      <PricingAndStats />
+      <SwimmingSchool />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
