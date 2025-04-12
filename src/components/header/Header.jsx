17a8ff0bc8c7b4758b4f636cbc1f710ef85c82e9
Suresh901 <sureshshrestha915@gmail.com>
2025-04-12 14:33:06 +0545

    header and navbar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { IoLocationSharp } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaPinterest } from "react-icons/fa";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center px-[30px] md:px-[60px] bg-[#0148bc] text-white text-[12px] md:text-[15px] ">
+      <div className="md:flex items-center md:space-x-4">
+        <div className="flex items-center space-x-2">
+          <MdEmail />
+          <h2>contact@example.com</h2>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 md:space-x-4 p-2">
+        <FaFacebook />
+        <FaTwitter />
+        <FaInstagram />
+        <FaPinterest />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className={`sticky top-0 left-0 w-full z-10 transition-transform duration-300 ease-in-out bg-white shadow-md
+      `}
+    >
+      <div className="flex justify-between items-center py-4 px-4 md:px-12 mx-auto max-w-[1500px]">
+        {/* Logo Section */}
+        <div className="flex-shrink-0 w-20 md:w-24">
+          <img src="image" alt="logo" className="w-full h-auto" />
+        </div>
+
+        {/* Navigation Links for Large Screens */}
+        <div className="hidden lg:flex gap-x-5 items-center font-medium text-gray-500">
+          <ul className="flex gap-x-10">
+            <li>Home</li>
+            <li>About</li>
+            <li>Courses</li>
+            <li>Instructor</li>
+            <li>Register</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        {/* Hamburger Menu and Dropdowns for Small Screens */}
+        <div className="lg:hidden flex items-center gap-2 sm:gap-3 md:gap-5">
+          <button className={`border p-2 rounded-md bg-white flex gap-2`}>
+            Join <span className="hidden sm:block"> Our Journey</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
